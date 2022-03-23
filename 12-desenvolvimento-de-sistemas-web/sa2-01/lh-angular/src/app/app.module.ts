47bd062e6c90150c73ef2ebffe0ca7ab91636ae1
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-22 21:45:30 -0300

    Added routing

--- 12-desenvolvimento-de-sistemas-web/sa2-01/lh-angular/src/app/app.module.ts
+++ 12-desenvolvimento-de-sistemas-web/sa2-01/lh-angular/src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MuralVagasComponent
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
